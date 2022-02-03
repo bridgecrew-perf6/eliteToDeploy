04991a4d88d34df4e725e477bd897c4193e0b9ec
cleroux84 <cleroux84@gmail.com>
2022-02-03 13:00:57 +0100

    first commit

---
+++ src/components/Articles.vue
@@ -0,0 +1,131 @@
+<template>
+  <div v-if="currentArticle" class="edit-form">
+    <h4>Article</h4>
+    <form>
+      <div class="form-group">
+        <label for="title">Title</label>
+        <input type="text" class="form-control" id="title"
+               v-model="currentArticle.title"
+        />
+      </div>
+      <div class="form-group">
+        <label for="description">Article</label>
+        <input type="text" class="form-control" id="description"
+               v-model="currentArticle.content"
+        />
+      </div>
+
+      <div class="form-group">
+        <label><strong>Status:</strong></label>
+        {{ currentArticle.published ? "Published" : "Pending" }}
+      </div>
+    </form>
+
+    <button class="badge badge-primary mr-2"
+            v-if="currentArticle.published"
+            @click="updatePublished(false)"
+    >
+      UnPublish
+    </button>
+    <button v-else class="badge badge-primary mr-2"
+            @click="updatePublished(true)"
+    >
+      Publish
+    </button>
+
+    <button class="badge badge-danger mr-2"
+            @click="deleteArticle"
+    >
+      Delete
+    </button>
+
+    <button type="submit" class="badge badge-success"
+            @click="updateArticle"
+    >
+      Update
+    </button>
+    <p>{{ message }}</p>
+  </div>
+
+  <div v-else>
+    <br />
+    <p>Please click on an article...</p>
+  </div>
+</template>
+
+<script>
+import DataService from "./service/DataService";
+
+export default {
+  name: "articles",
+  data() {
+    return {
+      currentArticle: null,
+      message: ''
+    };
+  },
+  methods: {
+    getArticle(id) {
+      DataService.get(id)
+          .then(response => {
+            this.currentArticle = response.data;
+            console.log(response.data);
+          })
+          .catch(e => {
+            console.log(e);
+          });
+    },
+
+    updatePublished(status) {
+      var data = {
+        id: this.currentArticle.id,
+        title: this.currentArticle.title,
+        description: this.currentArticle.content,
+        published: status
+      };
+
+      DataService.update(this.currentArticle.id, data)
+          .then(response => {
+            this.currentArticle.published = status;
+            console.log(response.data);
+          })
+          .catch(e => {
+            console.log(e);
+          });
+    },
+
+    updateArticle() {
+      DataService.update(this.currentArticle.id, this.currentArticle)
+          .then(response => {
+            console.log(response.data);
+            this.message = 'The article was updated successfully!';
+          })
+          .catch(e => {
+            console.log(e);
+          });
+    },
+
+    deleteArticle() {
+      DataService.delete(this.currentArticle.id)
+          .then(response => {
+            console.log(response.data);
+            this.$router.push({ name: "articles" });
+          })
+          .catch(e => {
+            console.log(e);
+          });
+    }
+  },
+  mounted() {
+    this.message = '';
+    this.getArticle(this.$route.params.id);
+  }
+};
+</script>
+
+<style>
+.edit-form {
+  max-width: 300px;
+  margin: auto;
+}
+</style>
