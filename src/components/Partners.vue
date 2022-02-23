77f7ad0bfb197297e3280397ee05e6876cceaf4a
cleroux84 <cleroux84@gmail.com>
2022-02-23 14:44:47 +0100

    page partner

--- src/components/Partners.vue
+++ src/components/Partners.vue
@@ -1,8 +1,34 @@
 <template>
-  <section>
-    <div style="color: #003f5f; background-color: #d9d9d9">
-      <h1 title="coaching développement personnel partenaire" style="font-family: Copperplate,serif; padding-left: 1%">PARTENARIAT</h1>
-
+  <section style="color: #003f5f; background-color: #d9d9d9">
+    <div >
+      <h1 title="coaching développement personnel partenaire" style="font-family: Copperplate,serif; padding-left: 1%">PARTENARIATS</h1>
     </div>
+    <v-container>
+      <h2 title="LES BIENFAITS D' ANNAMALAIYA" style="font-family: Copperplate,serif;">LES BIENFAITS D' ANNAMALAIYA</h2>
+      <v-layout row wrap align-center>
+        <v-flex class="text-xs-center" style="margin-top: 3%">
+          <v-img
+            :src="require('../assets/images/annabelle.jpeg')"
+            class="my-3"
+            contain
+            height="300"
+          />
+        </v-flex>
+      </v-layout>
+    </v-container>
+    <v-container>
+      <v-row style="font-family: Copperplate,serif;">
+        <v-col class="text-center" cols="12">
+          <v-row>Modelage bien-être ayurvédique et soin énergétique/Reiki accompagné des vertus des minéraux (lithothérapie) et de mon bol tibétain.</v-row>
+          <br>
+          <a  href="https://www.lesbienfaitsdannamalaiya.fr/" target="_blank" style="text-decoration: none; color: inherit;">
+            <v-icon color="#003f5f">mdi-cursor-pointer</v-icon>
+            <v-spacer></v-spacer>
+            <span>Visiter le site</span>
+          </a>
+        </v-col>
+      </v-row>
+    </v-container>
+
   </section>
 </template>
