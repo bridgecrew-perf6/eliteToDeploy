86773b9d4955ed16df6647ae838a6355275024d7
cleroux84 <cleroux84@gmail.com>
2022-02-28 13:41:08 +0100

    clean

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devServer: {
     proxy: 'https://afternoon-castle-40052.herokuapp.com',
     // proxy: 'http://localhost:5000',
-    port: 8081
+    // port: 8081
   },
   transpileDependencies: [
     'vuetify'
